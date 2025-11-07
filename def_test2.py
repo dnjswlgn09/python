57a34786b80cde93b83f17d47a4fabff090da95f
dnjswlgn09 <jwon81520@gmail.com>
2025-11-07 12:35:39 +0900

    Add files via upload

---
+++ def_test2.py
@@ -0,0 +1,20 @@
+#def_test3.py
+#함수
+#1. 내장함수 print() input()
+#2. 사용자 정의 함수 내가 def로 만든거
+#3. 라이브러리 함수 import를 이용해 사
+'''
+import time
+
+print("안녕")
+time.sleep(1)
+print("반가워!!")
+
+'''
+#카운트다운
+import time
+for i in range(10,0,-1):
+    print(i)
+    time.sleep(1)
+print("해피뉴이어")
+
